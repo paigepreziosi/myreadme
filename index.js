b016646dd9774def5d05c55f4d784825e1e01dfe
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-03 18:33:08 -0500

    commit questions

--- index.js
+++ index.js
@@ -4,7 +4,64 @@ const generateMarkdown = require("utils/generateMarkdown");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'What is your full name?',
+    name: 'name',
+},
+{
+    type: 'input',
+    message: 'Please provide a description of your project.',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'Please provide a step-by-step description of how to install your project.',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'Please provide a step-by-step description of how to use your project.',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: '',
+    name: 'contributing',
+},
+{
+    type: 'input',
+    message: 'Please provide a step-by-step description of how to test your project.',
+    name: 'test',
+},
+{
+    type: 'input',
+    message: 'What is your e-mail address?',
+    name: 'email',
+},
+{
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'github',
+},
+{
+    type: 'input',
+    message: 'What is your e-mail address?',
+    name: 'email',
+},
+{
+    type: 'list',
+    message: 'Please select a license.',
+    name: 'license',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
