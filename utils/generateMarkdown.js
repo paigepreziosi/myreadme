b08db0aaf29aabf4ba2e27d42997094706a39bb4
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-03 20:15:47 -0500

    commit markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,13 +6,38 @@ function renderLicenseBadge(license) {}
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  # $(data.title)
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [Questions](#questions)
+
+### Description
+  $(data.description)
+
+### Installation
+  $(data.installation)
+
+### Usage
+ $(data.usage)
+
+### Contributing
+ $(data.contributing)
+
+### Tests
+  $(data.test)
+
+### Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [github.com/${data.github}](https://github.com/${data.github}/).
 
 `;
 }
