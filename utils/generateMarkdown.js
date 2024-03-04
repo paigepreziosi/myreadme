0bd5e4431cfd638ca6a41eecd8b49da71552b38d
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-03 20:22:02 -0500

    license funtions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license == 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache)'
+  }
+  if (license == 'GPL') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if (license == 'BSD') {
+    return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD)'
+  }
+  if (license == 'None') {
+    return ''
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+}
 
 
 // TODO: Create a function to generate markdown for README
@@ -20,6 +33,7 @@ function generateMarkdown(data) {
   - [Contributing](#contributing)
   - [Test](#test)
   - [Questions](#questions)
+  ${data.license !== "None" ? "- [License](#license)" : ""}
 
 ### Description
   ${data.description}
@@ -39,6 +53,8 @@ function generateMarkdown(data) {
 ### Questions
   If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [github.com/${data.github}](https://github.com/${data.github}/).
 
+${data.license} !== 'None' ? '### License' : ''}
+${data.license} !== 'None' ? renderLicenseLink(data.license) : ''}
 `;
 }
 
