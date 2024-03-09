958d99fe880ce121d202c187c2bb3d7d65c12cf4
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-09 13:27:38 -0500

    fixed issues

--- GeneratedREADME.md
+++ GeneratedREADME.md
@@ -1,5 +1,5 @@
 
-  # 
+  # README Generator
 
   ## Table of Contents
   - [Description](#description)
@@ -11,23 +11,23 @@
   - [License](#license)
 
 ### Description
-  
+  This is a description of this project.
 
 ### Installation
-  
+  This is how to install this project.
 
 ### Usage
- 
+ This is how to use this project.
 
 ### Contributing
- 
+ This is how to contribute to this project.
 
 ### Tests
-  
+  This is how to test this this project.
 
 ### Questions
-  If you have any questions about the repo, open an issue or contact me directly at . You can find more of my work at [github.com/](https://github.com//).
+  If you have any questions about the repo, open an issue or contact me directly at PVPreziosi@hotmail.com. You can find more of my work at [github.com/paigepreziosi](https://github.com/paigepreziosi/).
   
   ### License
-  undefined
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache)
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 function renderLicenseBadge(license) {
   if (license == 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    return '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
-  if (license == 'Apache') {
+  if (license == 'Apache 2.0') {
     return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache)'
   }
-  if (license == 'GPL') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  if (license == 'GPL 3.0') {
+    return '[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
   }
-  if (license == 'BSD') {
+  if (license == 'BSD 3') {
     return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD)'
   }
   if (license == 'None') {
